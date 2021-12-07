75f41c977401d92f1555b0c83f0603a52ad05744
Molnar Emese <mse212002@gmail.com>
2021-12-07 17:40:45 +0200

    construction: added input field to add new todo item

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -6,16 +6,31 @@ class InputTodo extends Component {
   };
 
   onChange = (e) => {
-    console.log("hello");
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
   }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: ""
+      });
+    } else {
+      alert("Please add a valid item!")
+    }
+  };
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input 
           type="text"
           placeholder="Add Todo..."
           value={this.state.title}
+          name="title"
           onChange={this.onChange}
         />
         <button>Submit</button>
