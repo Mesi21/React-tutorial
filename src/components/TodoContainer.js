71a9d92172698181d3b6d3161fd7f9cf7c3ab548
Molnar Emese <mse212002@gmail.com>
2021-12-07 10:55:38 +0200

    :construction: Added InputTodo component, updated the TodoItem component

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodoList from './TodoList';
 import Header from './Header';
+import InputTodo from "./InputTodo";
 
 class TodoContainer extends React.Component {
   state = {
@@ -23,15 +24,40 @@ class TodoContainer extends React.Component {
     ]
    };
 
-  handleChange = () => {
-    console.log("clicked", this.id);
+  handleChange = (id) => {
+    this.setState(prevState => ({
+      todos: prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo;
+      })
+    }))
+  };
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    })
   }
    
   render() {
     return (
       <div>
         <Header />
-        <TodoList todos={this.state.todos} handleChangeProps={this.handleChange} />
+        <InputTodo />
+        <TodoList 
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+        />
       </div>
     )
   };
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,9 +7,12 @@ class TodoItem extends React.Component {
         <input
           type='checkbox'
           checked={this.props.todo.completed}
-          onChange={() => console.log("clicked")}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
         />
           {this.props.todo.title}
+          <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+            Delete
+          </button>
       </li>
     )
   }
