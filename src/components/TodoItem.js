879c677be90a096e7b8c7b4e6bbc0665135ef723
Molnar Emese <mse212002@gmail.com>
2021-12-07 17:49:15 +0200

    :art: added stylinh

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,18 +1,19 @@
 import React from "react";
+import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
   render() {
     return (
-      <li>
+      <li className={styles.item}>
         <input
           type='checkbox'
           checked={this.props.todo.completed}
           onChange={() => this.props.handleChangeProps(this.props.todo.id)}
         />
-          {this.props.todo.title}
-          <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
-            Delete
-          </button>
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        {this.props.todo.title}
       </li>
     )
   }
